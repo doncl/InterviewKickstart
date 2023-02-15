92f1986ea51774175f3388b108999cb327808245
Don Clore <cloredon42@gmail.com>
2023-02-14 17:51:23 -0800

    main ReviewAllSortsV11

---
+++ Sorts/SortingRelatedProblems/AttendMeetings/AttendMeetings/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  AttendMeetings
+//
+//  Created by Don Clore on 2/14/23.
+//
+
+import Foundation
+
+func can_attend_all_meetings(intervals: [[Int]]) -> Int {
+    // Write your code here.
+    return 0
+}
+
+
+let intervals: [[Int]] = [[1, 5,], [5, 8,], [10, 15,],]
+
+
