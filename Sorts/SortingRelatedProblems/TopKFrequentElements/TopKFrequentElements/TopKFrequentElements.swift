a155ab08bf07a1fe48eaacd1a83d33b4319e2751
Don Clore <cloredon42@gmail.com>
2023-02-20 11:22:53 -0800

    main - TopKFrequentElements

--- Sorts/SortingRelatedProblems/TopKFrequentElements/TopKFrequentElements/TopKFrequentElements.swift
+++ Sorts/SortingRelatedProblems/TopKFrequentElements/TopKFrequentElements/TopKFrequentElements.swift
@@ -9,9 +9,7 @@ func find_top_k_frequent_elements(arr: [Int], k: Int) -> [Int] {
   let map = arr.reduce(into: [:]) { dict, num in
     dict[num, default: 0] += 1
   }
-  
-  var numbers = map.map { $0.key }
-    
+      
   let numbers = map.sorted(by: { lhs, rhs in
     lhs.value > rhs.value
   }).map { $0.key }
