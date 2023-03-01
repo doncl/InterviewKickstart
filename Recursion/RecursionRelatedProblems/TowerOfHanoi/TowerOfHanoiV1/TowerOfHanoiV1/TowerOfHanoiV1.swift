3a5cad249b995ff76bd002e48b13ad2dce24a947
Don Clore <cloredon42@gmail.com>
2023-03-01 10:16:52 -0800

    main - ReviewAllSortsV27

---
+++ Recursion/RecursionRelatedProblems/TowerOfHanoi/TowerOfHanoiV1/TowerOfHanoiV1/TowerOfHanoiV1.swift
@@ -0,0 +1,21 @@
+//
+//  TowerOfHanoiV1.swift
+//  TowerOfHanoiV1
+//
+//  Created by Don Clore on 2/28/23.
+//
+
+//func towerOfHanoi(n: Int, src: Int, dst: Int, aux: Int) {
+//  if n == 1 {
+//    print("Move disk from \(src) to \(dst)")
+//  } else {
+//    towerOfHanoi(n: n - 1, src: src, dst: aux, aux: dst)
+//    print("Move disk from \(src) to \(dst)")
+//    towerOfHanoi(n: n - 1, src: aux, dst: dst, aux: src)
+//  }
+//}
+
+func tower_of_hanoi(n: Int) -> [[Int]] {
+  
+  return []
+}
---
+++ Recursion/RecursionRelatedProblems/TowerOfHanoi/TowerOfHanoiV1/TowerOfHanoiV1Tests/TowerOfHanoiV1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TowerOfHanoiV1Tests.swift
+//  TowerOfHanoiV1Tests
+//
+//  Created by Don Clore on 2/28/23.
+//
+
+import XCTest
+@testable import TowerOfHanoiV1
+
+final class TowerOfHanoiV1Tests: XCTestCase {
+  func testExample() {
+    let n = 4
+    let ret = tower_of_hanoi(n: n)
+    
+    let expectedArray: [[Int]] = [
+      [1, 2],
+      [1, 3],
+      [2, 3],
+      [1, 2],
+      [3, 1],
+      [3, 2],
+      [1, 2],
+      [1, 3],
+      [2, 3],
+      [2, 1],
+      [3, 1],
+      [2, 3],
+      [1, 2],
+      [1, 3],
+      [2, 3]
+    ]
+    
+    XCTAssertEqual(ret, expectedArray)    
+  }
+}
