7efb2c5bc6bdb05f5c7e02b81edb2717237b3177
Don Clore <cloredon42@gmail.com>
2023-03-07 13:06:43 -0700

    main - NChooseKCombinationsV4

---
+++ Recursion/RecursionRelatedProblems/NChooseK/NChooseKCombinationsV4/NChooseKCombinationsV4/NChooseKCombinationsV4.swift
@@ -0,0 +1,36 @@
+//
+//  NChooseKCombinationsV4.swift
+//  NChooseKCombinationsV4
+//
+//  Created by Don Clore on 3/7/23.
+//
+
+func find_combinations(n: Int, k: Int) -> [[Int]] {
+  var ret: [[Int]] = []
+  var array: [Int] = []
+  
+  for i in 1...n {
+    array.append(i)
+  }
+  
+  func helper(slate: inout [Int], index: Int, kVal: Int) {
+    guard kVal > 0 else {
+      ret.append(slate)
+      return
+    }
+    
+    for i in index..<array.count {
+      
+      let candidate = array[i]
+      slate.append(candidate)
+      helper(slate: &slate, index: i + 1, kVal: kVal - 1)
+      _ = slate.popLast()
+    }
+  }
+  
+  var slate: [Int] = []
+
+   helper(slate: &slate, index: 0, kVal: k)
+  return ret 
+}
+
