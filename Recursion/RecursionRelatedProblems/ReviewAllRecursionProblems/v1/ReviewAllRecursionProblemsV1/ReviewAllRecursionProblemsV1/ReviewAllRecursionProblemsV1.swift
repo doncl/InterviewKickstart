eedc502a27e7628c046d850c83063997359d27ab
Don Clore <cloredon42@gmail.com>
2023-03-08 14:55:20 -0700

    ReviewAllRecursionProblemsV1

--- Recursion/RecursionRelatedProblems/ReviewAllRecursionProblems/v1/ReviewAllRecursionProblemsV1/ReviewAllRecursionProblemsV1/ReviewAllRecursionProblemsV1.swift
+++ Recursion/RecursionRelatedProblems/ReviewAllRecursionProblems/v1/ReviewAllRecursionProblemsV1/ReviewAllRecursionProblemsV1/ReviewAllRecursionProblemsV1.swift
@@ -5,6 +5,7 @@
 //  Created by Don Clore on 3/8/23.
 //
 
+// MARK: Fibonacci
 func find_fibonacci(n: Int) -> Int {
   func helper(n: Int, base1: Int, base2: Int) -> Int {
     if n == 0 {
@@ -13,4 +14,227 @@ func find_fibonacci(n: Int) -> Int {
     return helper(n: n - 1, base1: base2, base2: base1 + base2)
   }
   return helper(n: n, base1: 0, base2: 1)
+}
+
+// MARK: NChooseK
+func find_combinations(n: Int, k: Int) -> [[Int]] {
+  var ret: [[Int]] = []
+  var array: [Int] = []
+  for i in 1...n {
+    array.append(i)
+  }
+  
+  func helper(slate: inout [Int], index: Int, kVal: Int) {
+    guard kVal > 0 else {
+      ret.append(slate)
+      return
+    }
+    
+    for i in index..<array.count {
+      let candidate = array[i]
+      slate.append(candidate)
+      helper(slate: &slate, index: i + 1, kVal: kVal - 1)
+      _ = slate.popLast()
+    }
+  }
+  
+  var slate: [Int] = []
+  
+  helper(slate: &slate, index: 0, kVal: k)
+  
+  return ret
+}
+
+// MARK: TowersOfHanoi
+func towers_of_hanoi(n: Int) -> [[Int]] {
+  var ret: [[Int]] = []
+  
+  func helper(n: Int, src: Int, dst: Int, aux: Int) {
+    guard n > 0 else {
+      return
+    }
+    
+    helper(n: n - 1, src: src, dst: aux, aux: dst)
+    ret.append([src, dst])
+    helper(n: n - 1, src: aux, dst: dst, aux: src)
+  }
+  
+  helper(n: n, src: 1, dst: 3, aux: 2)
+  return ret
+}
+
+// MARK: BinaryStrings
+func binaryStrings(n: Int) -> [String] {
+  var ret: [String] = []
+  
+  guard n > 0 else {
+    return ret
+  }
+  
+  func helper(slate: String) {
+    guard slate.count < n else {
+      ret.append(slate)
+      return
+    }
+    
+    helper(slate: slate + "0")
+    helper(slate: slate + "1")
+  }
+  
+  let slate: String = ""
+  helper(slate: slate)
+  
+  return ret
+}
+
+// MARK: DecimalStringsNoRepetition
+func decimalStringsNoRepetition(n: Int) -> [String] {
+  var ret: [String] = []
+  
+  var digits: String = "0123456789"
+  
+  func helper(build: inout String, digits: inout String) {
+    guard build.count < n else {
+      ret.append(build)
+      return
+    }
+    
+    for i in 0..<digits.count {
+      let stringIndex = digits.index(digits.startIndex, offsetBy: i)
+      let candidate = digits[stringIndex]
+      
+      build.append(candidate)
+      digits.remove(at: stringIndex)
+      
+      helper(build: &build, digits: &digits)
+      
+      digits.insert(candidate, at: stringIndex)
+      _ = build.popLast()
+    }
+  }
+  
+  var build: String = ""
+  
+  helper(build: &build, digits: &digits)
+
+  return ret
+}
+
+// MARK: DecimalStringsAllowRepetition
+func decimalStringsAllowRepetition(n: Int) -> [String] {
+  var ret: [String] = []
+  
+  let digits: String = "0123456789"
+  
+  func helper(build: inout String) {
+    guard build.count < n else {
+      ret.append(build)
+      return
+    }
+    
+    for i in 0..<digits.count {
+      let stringIndex: String.Index = digits.index(digits.startIndex, offsetBy: i)
+      let candidate = digits[stringIndex]
+      build.append(candidate)
+      
+      helper(build: &build)
+      
+      _ = build.popLast()
+    }
+  }
+  
+  var build = ""
+  helper(build: &build)
+  return ret 
+}
+
+// MARK: PermuteIntsWithoutRepetition
+func permuteIntsWithoutRepetitions(arr: [Int]) -> [[Int]] {
+  var ret: [[Int]] = []
+  
+  func helper(build: inout [Int], bank: inout [Int]) {
+    guard build.count < arr.count else {
+      ret.append(build)
+      return
+    }
+    let count = bank.count
+    for i in 0..<count {
+      let candidate = bank[i]
+      build.append(candidate)
+      bank.remove(at: i)
+      
+      helper(build: &build, bank: &bank)
+      
+      bank.insert(candidate, at: i)
+      _ = build.popLast()
+    }
+  }
+  
+  var build: [Int] = []
+  var bank = arr
+  
+  helper(build: &build, bank: &bank)
+  return ret
+}
+
+// MARK: PermuteStringWithoutRepetition
+func permuteStringWithoutRepetition(s: String) -> [String] {
+  var ret: [String] = []
+  
+  func helper(build: inout String, bank: inout String) {
+    guard build.count < s.count else {
+      ret.append(build)
+      return
+    }
+    
+    let count = bank.count
+    for i in 0..<count {
+      let stringIndex: String.Index = bank.index(bank.startIndex, offsetBy: i)
+      let candidate = bank[stringIndex]
+      
+      build.append(candidate)
+      bank.remove(at: stringIndex)
+      
+      helper(build: &build, bank: &bank)
+      
+      bank.insert(candidate, at: stringIndex)
+      _ = build.popLast()
+    }
+  }
+  
+  var build: String = ""
+  var bank = s
+
+  helper(build: &build, bank: &bank)
+  return ret
+}
+
+// MARK: GenerateStringSubsets
+func generateAllStringSubsets(s: String) -> [String] {
+  var ret: [String] = []
+  
+  func helper(build: String, bank: String) {
+    guard !bank.isEmpty else {
+      ret.append(build)
+      return
+    }
+  
+    let oneFromZero: String.Index = bank.index(bank.startIndex, offsetBy: 1)
+    
+    let stringWithFirstCharacterExcluded = String(bank[oneFromZero...])
+    let stringOfFirstCharacter = String(bank[bank.startIndex..<oneFromZero])
+    
+    // exclude
+    helper(build: build, bank: stringWithFirstCharacterExcluded)
+    
+    //include
+    var newBuild = build
+    newBuild.append(stringOfFirstCharacter)
+    helper(build: newBuild, bank: stringWithFirstCharacterExcluded)
+  }
+  
+  let build: String = ""
+  let bank = s
+  helper(build: build, bank: bank)
+  return ret
 }
--- Recursion/RecursionRelatedProblems/ReviewAllRecursionProblems/v1/ReviewAllRecursionProblemsV1/ReviewAllRecursionProblemsV1Tests/ReviewAllRecursionProblemsV1Tests.swift
+++ Recursion/RecursionRelatedProblems/ReviewAllRecursionProblems/v1/ReviewAllRecursionProblemsV1/ReviewAllRecursionProblemsV1Tests/ReviewAllRecursionProblemsV1Tests.swift
@@ -5,10 +5,13 @@
 //  Created by Don Clore on 3/8/23.
 //
 
+
 import XCTest
 @testable import ReviewAllRecursionProblemsV1
 
 final class ReviewAllRecursionProblemsV1Tests: XCTestCase {
+  // MARK: Fibonacci
+  
   func testFib() {
     var fibs: [Int] = []
     for i in 1...10 {
@@ -23,5 +26,464 @@ final class ReviewAllRecursionProblemsV1Tests: XCTestCase {
     XCTAssertEqual(fibs, expected)
   }
 
+  // MARK: NChooseK
+  func testNChooseKExample1() {
+    let result = find_combinations(n: 5, k: 2)
+    
+    let expected = Set<[Int]>([
+      [1, 2],
+      [1, 3],
+      [1, 4],
+      [1, 5],
+      [2, 3],
+      [2, 4],
+      [2, 5],
+      [3, 4],
+      [3, 5],
+      [4, 5],
+    ])
+    
+    XCTAssertEqual(result.count, expected.count)
+  
+    let resultSet = Set<[Int]>(result)
+    
+    XCTAssertEqual(resultSet, expected)
+    
+  }
+  
+  func testNChooseKExample2() {
+    let result = find_combinations(n: 6, k: 6)
+    XCTAssertEqual(result.count, 1)
+    
+    let resultSet = Set<Int>(result[0])
+    let expectedSet = Set<Int>([1, 2, 3, 4, 5, 6])
+    
+    XCTAssertEqual(resultSet, expectedSet)
+  }
+  
+  func testNChooseK2NumExample() {
+    let result = find_combinations(n: 2, k: 2)
+    XCTAssertEqual(result.count, 1)
+    
+    let resultSet = Set<Int>(result[0])
+    let expectedSet = Set<Int>([1, 2,])
+    
+    XCTAssertEqual(resultSet, expectedSet)
+  }
+
+  
+  func testNChooseKFailingExample() {
+    let result = find_combinations(n: 15, k: 5)
+    print(result)
+  }
+  
+  func testNChooseK20FailingExample() {
+    let result = find_combinations(n: 20, k: 20)
+
+    XCTAssertEqual(result.count, 1)
+    
+    let resultSet = Set<Int>(result[0])
+    let expectedSet = Set<Int>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+    
+    XCTAssertEqual(resultSet, expectedSet)
+  }
+
+  // MARK: TowersOfHanoi
+  func testTowersOfHanoiExample() {
+    let n = 4
+    let ret = towers_of_hanoi(n: n)
+    
+    let expectedArray: [[Int]] = [
+      [1, 2],
+      [1, 3],
+      [2, 3],
+      [1, 2],
+      [3, 1],
+      [3, 2],
+      [1, 2],
+      [1, 3],
+      [2, 3],
+      [2, 1],
+      [3, 1],
+      [2, 3],
+      [1, 2],
+      [1, 3],
+      [2, 3]
+    ]
+    
+    XCTAssertEqual(ret, expectedArray)
+  }
+  
+  func testTowersOfHanoiExample2() {
+    let n = 3
+    let ret = towers_of_hanoi(n: n)
+    
+    let expectedArray: [[Int]] = [
+      [1, 3],
+      [1, 2],
+      [3, 2],
+      [1, 3],
+      [2, 1],
+      [2, 3],
+      [1, 3],
+    ]
+    
+    XCTAssertEqual(ret, expectedArray)
+  }
+  
+  // MARK: BinaryStrings
+  
+  func testBinaryStringsExample1() {
+    let ret = binaryStrings(n:3)
+    let expected = [
+      "000",
+      "001",
+      "010",
+      "011",
+      "100",
+      "101",
+      "110",
+      "111",
+    ]
+    
+    XCTAssertEqual(expected, ret)
+  }
+  
+  func testBinaryStringsForSize0() {
+    var ret = binaryStrings(n: 0)
+    XCTAssertTrue(ret.isEmpty)
+  }
+  
+  func testBinaryStringsForSize1() {
+    var ret = binaryStrings(n: 1)
+    let expected = [
+      "0",
+      "1",
+    ]
+    
+    XCTAssertEqual(ret, expected)
+  }
+  
+  // MARK: DecimalStringsNoRepetition
+  func testDecimalStringsNoRepetitionLength1() {
+    let ret = decimalStringsNoRepetition(n: 1)
+    let expected = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+    ]
+    
+    XCTAssertEqual(ret, expected)
+  }
+  
+  func testDecimalStringsNoRepetitionLength2() {
+    let ret = decimalStringsNoRepetition(n: 2)
+    let expected = [
+      "01",
+      "02",
+      "03",
+      "04",
+      "05",
+      "06",
+      "07",
+      "08",
+      "09",
+      
+      "10",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      
+      "20",
+      "21",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      
+      "30",
+      "31",
+      "32",
+      "34",
+      "35",
+      "36",
+      "37",
+      "38",
+      "39",
+      
+      "40",
+      "41",
+      "42",
+      "43",
+      "45",
+      "46",
+      "47",
+      "48",
+      "49",
+      
+      "50",
+      "51",
+      "52",
+      "53",
+      "54",
+      "56",
+      "57",
+      "58",
+      "59",
+      
+      "60",
+      "61",
+      "62",
+      "63",
+      "64",
+      "65",
+      "67",
+      "68",
+      "69",
+      
+      "70",
+      "71",
+      "72",
+      "73",
+      "74",
+      "75",
+      "76",
+      "78",
+      "79",
+      
+      "80",
+      "81",
+      "82",
+      "83",
+      "84",
+      "85",
+      "86",
+      "87",
+      "89",
+      
+      "90",
+      "91",
+      "92",
+      "93",
+      "94",
+      "95",
+      "96",
+      "97",
+      "98",
+    ]
+    
+    XCTAssertEqual(ret, expected)
+  }
+  
+  // MARK: DecimalStringsAllowRepetition
+  func testDecimalStringsAllowRepetitionLength1() {
+    let ret = decimalStringsAllowRepetition(n: 1)
+    let expected = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+    ]
+    
+    XCTAssertEqual(ret, expected)
+  }
+  
+  func testDecimalStringsAllowRepetitionLength2() {
+    let ret = decimalStringsAllowRepetition(n: 2)
+    let expected = [
+      "00",
+      "01",
+      "02",
+      "03",
+      "04",
+      "05",
+      "06",
+      "07",
+      "08",
+      "09",
+      
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      
+      "20",
+      "21",
+      "22",
+      "23",
+      "24",
+      "25",
+      "26",
+      "27",
+      "28",
+      "29",
+      
+      "30",
+      "31",
+      "32",
+      "33",
+      "34",
+      "35",
+      "36",
+      "37",
+      "38",
+      "39",
+      
+      "40",
+      "41",
+      "42",
+      "43",
+      "44",
+      "45",
+      "46",
+      "47",
+      "48",
+      "49",
+      
+      "50",
+      "51",
+      "52",
+      "53",
+      "54",
+      "55",
+      "56",
+      "57",
+      "58",
+      "59",
+      
+      "60",
+      "61",
+      "62",
+      "63",
+      "64",
+      "65",
+      "66",
+      "67",
+      "68",
+      "69",
+      
+      "70",
+      "71",
+      "72",
+      "73",
+      "74",
+      "75",
+      "76",
+      "77",
+      "78",
+      "79",
+      
+      "80",
+      "81",
+      "82",
+      "83",
+      "84",
+      "85",
+      "86",
+      "87",
+      "88",
+      "89",
+      
+      "90",
+      "91",
+      "92",
+      "93",
+      "94",
+      "95",
+      "96",
+      "97",
+      "98",
+      "99",
+    ]
+    
+    XCTAssertEqual(ret, expected)
+  }
+  
+  
+  // MARK: PermuteIntsWithoutRepetitions
+  func testPermuteIntsWithoutRepetitions123() {
+    let array = [1, 2, 3,]
+    
+    let ret = permuteIntsWithoutRepetitions(arr: array)
+    
+    let expect = [
+      [1,2,3,],
+      [1,3,2,],
+      [2,1,3,],
+      [2,3,1,],
+      [3,1,2,],
+      [3,2,1,],
+    ]
+      
+    XCTAssertEqual(expect, ret)
+  }
+  
+  // MARK: PermuteStringWithoutRepetition
+  func testABC() {
+    let string = "ABC"
+    
+    let ret = permuteStringWithoutRepetition(s: string)
+    
+    let expect = [
+      "ABC",
+      "ACB",
+      "BAC",
+      "BCA",
+      "CAB",
+      "CBA",
+      ]
+      
+      XCTAssertEqual(expect, ret)
+  }
+  
+  // MARK: GenerateStringSubsets
+  func testGenerateStringSubsets123() {
+    let ret = generateAllStringSubsets(s: "123")
+    
+    let retSet = Set<String>(ret)
+    let expectSet = Set<String>([
+      "",
+      "3",
+      "2",
+      "23",
+      "1",
+      "13",
+      "12",
+      "123",
+    ])
+    
+    XCTAssertEqual(retSet, expectSet)
+  }
 
 }
+
+
+
+
