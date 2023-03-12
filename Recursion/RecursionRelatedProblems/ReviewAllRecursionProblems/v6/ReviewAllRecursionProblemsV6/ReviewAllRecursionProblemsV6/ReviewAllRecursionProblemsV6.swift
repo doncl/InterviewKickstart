818d5cb44b8369a1bd521ceb6f3de8bf6eb51932
Don Clore <cloredon42@gmail.com>
2023-03-12 13:49:55 -0600

    main - adding ReviewAllRecursionProblemsV6

---
+++ Recursion/RecursionRelatedProblems/ReviewAllRecursionProblems/v6/ReviewAllRecursionProblemsV6/ReviewAllRecursionProblemsV6/ReviewAllRecursionProblemsV6.swift
@@ -0,0 +1,117 @@
+//
+//  ReviewAllRecursionProblemsV6.swift
+//  ReviewAllRecursionProblemsV6
+//
+//  Created by Don Clore on 3/12/23.
+//
+
+// MARK: Fibonacci
+func find_fibonacci(n: Int) -> Int {
+ return 0
+}
+
+// MARK: NChooseK
+func find_combinations(n: Int, k: Int) -> [[Int]] {
+  var ret: [[Int]] = []
+  
+  return ret
+}
+
+// MARK: TowersOfHanoi
+func towers_of_hanoi(n: Int) -> [[Int]] {
+  var ret: [[Int]] = []
+  
+  
+  return ret
+}
+
+// MARK: BinaryStrings
+func binaryStrings(n: Int) -> [String] {
+  var ret: [String] = []
+  
+ 
+  
+  return ret
+}
+
+// MARK: DecimalStringsNoRepetition
+func decimalStringsNoRepetition(n: Int) -> [String] {
+  var ret: [String] = []
+  
+ 
+
+  return ret
+}
+
+// MARK: DecimalStringsAllowRepetition
+func decimalStringsAllowRepetition(n: Int) -> [String] {
+  var ret: [String] = []
+  
+ 
+  return ret
+}
+
+// MARK: PermuteIntsWithoutRepetition
+func permuteIntsWithoutRepetitions(arr: [Int]) -> [[Int]] {
+  var ret: [[Int]] = []
+  
+  
+  return ret
+}
+
+// MARK: PermuteIntsAllowRepetitions
+func permuteIntsAllowRepetitions(arr: [Int]) -> [[Int]] {
+  var ret: [[Int]] = []
+  
+  return ret
+}
+
+// MARK: PermuteStringWithoutRepetition
+func permuteStringWithoutRepetition(s: String) -> [String] {
+  var ret: [String] = []
+  
+ 
+  return ret
+}
+
+// MARK: PermuteStringAllowRepetitions
+func permuteStringAllowRepetitions(s: String) -> [String] {
+  var ret: [String] = []
+  
+  return ret
+}
+
+// MARK: GenerateStringSubsets
+func generateAllStringSubsets(s: String) -> [String] {
+  var ret: [String] = []
+  
+
+  return ret
+}
+
+// MARK: GenerateAllIntSubsets
+func generateAllIntSubsets(arr: [Int]) -> [[Int]] {
+  var ret: [[Int]] = []
+  
+  return ret
+}
+
+// MARK: GenerateIntSubsetAllowDuplicates
+func generateIntSubsetsAllowDuplicates(arr: [Int]) -> [[Int]] {
+  var ret: [[Int]] = []
+  
+  return ret
+}
+
+// MARK: Letter case Permutation
+// Given a string s, we can transform every letter individually to be lowercase or
+// uppercase to create another string.  Return a list of all possible strings we
+// could create.
+
+func letter_case_permutation(s: String) -> [String] {
+  var ret: [String] = []
+  
+
+  return ret
+}
+
