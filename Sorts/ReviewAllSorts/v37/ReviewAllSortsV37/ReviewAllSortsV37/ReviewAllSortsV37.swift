af0a1dd0ab463f7afd7e98191c12945e80b2fc2b
Don Clore <cloredon42@gmail.com>
2023-03-12 13:21:19 -0600

    main -ReviewAllSortsV37Setup

---
+++ Sorts/ReviewAllSorts/v37/ReviewAllSortsV37/ReviewAllSortsV37/ReviewAllSortsV37.swift
@@ -0,0 +1,39 @@
+//
+//  ReviewAllSortsV37.swift
+//  ReviewAllSortsV37
+//
+//  Created by Don Clore on 3/12/23.
+//
+
+func selectionSort(numbers: inout [Int]) {
+  
+}
+
+func bubbleSort(numbers: inout [Int]) {
+  
+}
+
+func insertionSort(numbers: inout [Int]) {
+}
+
+func mergeSort(numbers: inout [Int]) {
+  
+}
+
+func quickSortLomuto(numbers: inout [Int]) {
+  
+}
+
+func quickSortHoare(numbers: inout [Int]) {
+  
+}
+
+func heapSort(numbers: inout [Int]) {
+  
+}
+
+func dutch_flag_sort(balls: inout [Character]) {
+}
+
+func segregate_evens_and_odds(numbers: inout [Int]) {
+}
