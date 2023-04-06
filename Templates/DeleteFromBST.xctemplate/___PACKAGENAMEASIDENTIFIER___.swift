7cd4a9b8f9912a1366f57d64922fdba06ecbfd09
Don Clore <cloredon42@gmail.com>
2023-04-06 12:34:06 -0700

    main - DeleteFromBSTV2

---
+++ Templates/DeleteFromBST.xctemplate/___PACKAGENAMEASIDENTIFIER___.swift
@@ -0,0 +1,17 @@
+//___FILEHEADER___
+final class BinaryTreeNode {
+    var value: Int
+    var left: BinaryTreeNode?
+    var right: BinaryTreeNode?
+
+    public init(value: Int) {
+        self.value = value
+    }
+}
+
+func delete_from_bst(root: BinaryTreeNode?, values_to_be_deleted: [Int]) -> BinaryTreeNode? {
+  var ret = root
+  
+  return ret
+}
+
